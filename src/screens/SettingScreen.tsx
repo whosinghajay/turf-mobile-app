acd1c514de7726c77c35b006005feb8e78f9a1b6
unknown <whosinghajay@gmail.com>
2024-06-12 15:29:17 +0530

    user profile page created and toggle button has been created in setting page for location field

--- src/screens/SettingScreen.tsx
+++ src/screens/SettingScreen.tsx
@@ -1,11 +1,113 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, TouchableHighlight, Alert} from 'react-native';
+import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {default as LeftArrowIcon} from 'react-native-vector-icons/AntDesign';
+import {default as RightArrowIcon} from 'react-native-vector-icons/MaterialIcons';
+import {default as LogoutIcon} from 'react-native-vector-icons/MaterialCommunityIcons';
+import {default as ToggleIcon} from 'react-native-vector-icons/FontAwesome6';
 
 const SettingScreen = () => {
+  const [toggle, setToggle] = useState(false);
+  const navigation = useNavigation<any>();
+
+  const toggleLocationOnOff = () => {
+    setToggle(!toggle);
+  };
   return (
-    <View>
-      <Text>SettingScreen</Text>
-    </View>
+    <>
+      {/* header */}
+      <View className="flex-row justify-between mt-4 mx-4">
+        <View className="flex-row gap-2 items-center">
+          <TouchableHighlight
+            onPress={() => navigation.navigate('Home')}
+            underlayColor={'#EFEFEF'}>
+            <LeftArrowIcon name="arrowleft" size={23} color="#000000" />
+          </TouchableHighlight>
+          <Text className="text-black text-[18px] font-semibold">Settings</Text>
+        </View>
+      </View>
+
+      {/* setting options */}
+      <View className="mt-4 mx-4">
+        {/* language */}
+        <TouchableHighlight
+          onPress={() => Alert.alert('clicked')}
+          underlayColor={'#EFEFEF'}>
+          <View className="flex-row items-center justify-between border-b-2 pb-2 my-2 border-slate-200">
+            <Text className="text-black text-base">Language</Text>
+            <RightArrowIcon name="arrow-forward-ios" size={18} color="black" />
+          </View>
+        </TouchableHighlight>
+
+        {/* location */}
+        <TouchableHighlight
+          onPress={() => Alert.alert('clicked')}
+          underlayColor={'#EFEFEF'}>
+          <View className="flex-row items-center justify-between border-b-2 pb-2 my-2 border-slate-200">
+            <Text className="text-black text-base">Location</Text>
+            <TouchableHighlight
+              underlayColor={'transparent'}
+              onPress={toggleLocationOnOff}>
+              <ToggleIcon
+                name={toggle ? "toggle-on" : "toggle-off"}
+                size={26}
+                color="#0064D2"
+              />
+            </TouchableHighlight>
+          </View>
+        </TouchableHighlight>
+
+        {/* privacy policy */}
+        <TouchableHighlight
+          onPress={() => Alert.alert('clicked')}
+          underlayColor={'#EFEFEF'}>
+          <View className="flex-row items-center justify-between border-b-2 pb-2 my-2 border-slate-200">
+            <Text className="text-black text-base">Privacy Policy</Text>
+            <RightArrowIcon name="arrow-forward-ios" size={18} color="black" />
+          </View>
+        </TouchableHighlight>
+
+        {/* terms and condition */}
+        <TouchableHighlight
+          onPress={() => Alert.alert('clicked')}
+          underlayColor={'#EFEFEF'}>
+          <View className="flex-row items-center justify-between border-b-2 pb-2 my-2 border-slate-200">
+            <Text className="text-black text-base">Terms and Condition</Text>
+            <RightArrowIcon name="arrow-forward-ios" size={18} color="black" />
+          </View>
+        </TouchableHighlight>
+
+        {/* rate app */}
+        <TouchableHighlight
+          onPress={() => Alert.alert('clicked')}
+          underlayColor={'#EFEFEF'}>
+          <View className="flex-row items-center justify-between border-b-2 pb-2 my-2 border-slate-200">
+            <Text className="text-black text-base">Rate App</Text>
+            <Text className="text-black text-base">v4.87.2</Text>
+          </View>
+        </TouchableHighlight>
+
+        {/* delete account */}
+        <TouchableHighlight
+          onPress={() => Alert.alert('clicked')}
+          underlayColor={'#EFEFEF'}>
+          <View className="flex-row items-center justify-between border-b-2 pb-2 my-2 border-slate-200">
+            <Text className="text-black text-base">Delete Account</Text>
+            <RightArrowIcon name="arrow-forward-ios" size={18} color="black" />
+          </View>
+        </TouchableHighlight>
+
+        {/* logout */}
+        <TouchableHighlight
+          onPress={() => Alert.alert('clicked')}
+          underlayColor={'#EFEFEF'}>
+          <View className="flex-row items-center justify-between border-b-2 pb-2 my-2 border-slate-200">
+            <Text className="text-black text-base">Logout</Text>
+            <LogoutIcon name="logout" size={20} color="black" />
+          </View>
+        </TouchableHighlight>
+      </View>
+    </>
   );
 };
 
--- src/screens/UserProfileScreen.tsx
+++ src/screens/UserProfileScreen.tsx
@@ -1,12 +1,67 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  TouchableHighlight,
+  Alert,
+} from 'react-native';
+import React from 'react';
+import {default as ShareIcon} from 'react-native-vector-icons/FontAwesome';
 
 const UserProfileScreen = () => {
   return (
-    <View>
-      <Text>UserProfileScreen</Text>
-    </View>
-  )
-}
+    <>
+      {/* header */}
+      <View className="bg-[#1D1CA3] px-4 pt-4 rounded-b-[30px]">
+        <Text className="text-white font-semibold">My Profile</Text>
+        <View className="mx-auto mt-10 mb-8">
+          <Image
+            source={require('../assests/images/profileImage.png')}
+            style={{width: 92.82, height: 92.82}}
+          />
+          <View className='absolute top-[72px] left-[69px] bg-white rounded-full p-[2px]'>
+            <ShareIcon name="share-square-o" size={14} color="black" />
+          </View>
+        </View>
+      </View>
 
-export default UserProfileScreen
+      {/* info section */}
+      <View className="mx-auto mt-6">
+        <View className="mb-4">
+          <Text className="text-black text-base font-semibold mb-1">
+            Enter Your Full Name
+          </Text>
+          <TextInput
+            placeholder="Enter Your Full Name"
+            className="border-[1.5px] rounded-lg border-black text-black pl-5"
+            style={{width: 310, height: 50}}
+            placeholderTextColor="black"
+          />
+        </View>
+
+        <View>
+          <Text className="text-black text-base font-semibold mb-1">
+            Enter Your Full Name
+          </Text>
+          <TextInput
+            placeholder="Enter Your Location"
+            className="border-[1.5px] rounded-lg border-black text-black pl-5"
+            style={{width: 310, height: 50}}
+            placeholderTextColor="black"
+          />
+        </View>
+
+        <TouchableHighlight
+          underlayColor={'transparent'}
+          onPress={() => Alert.alert('You clicked on SAVE button')}>
+          <View className="bg-[#1D1CA3] mt-6 rounded-xl">
+            <Text className="text-white text-center py-3 text-base">Save</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
+    </>
+  );
+};
+
+export default UserProfileScreen;
